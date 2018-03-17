d9daa6d2ef6983ef6c874c2aed5b60a150e4d8df
Dhammikalks <dhammika.lks@gmail.com>
2018-03-17 19:50:02 +0530

    adding back ends of user interactive

---
+++ logtime.php
@@ -0,0 +1,12 @@
+<?php //logtime.php
+if(isset($_POST['X_post']))
+{
+    $x = $_POST['X_post'];
+    $y = $_POST['Y_post'];
+
+    echo $x;
+    echo ",";
+    echo $y;
+    // Do whatever you want with the $uid
+}
+?>
