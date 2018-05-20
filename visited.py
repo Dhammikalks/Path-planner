139706204519e1ddb6c890658fceb6a0fd2645cf
Dhammikalks <dhammika.lks@gmail.com>
2018-05-20 10:03:08 +0530

    resolving node issue

--- visited.py
+++ visited.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python -f
+#!/usr/bin/env python3
 import Pyro4
+import sys
+
 control = Pyro4.Proxy("PYRONAME:example.control");
-
-nodes = control.get_Vector()
-
+nodes = control.get_Vector();
 print(nodes);
-
